af51ca51b80c966d6bd1892fd9db87fe3b9b5ec9
Aditya Jawanjal <adityajawanjal01@gmail.com>
2023-06-29 12:04:27 +0530

    m

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,8 +1,8 @@
 const multer = require("multer");
 
-exports.uploadProfilePic = multer();
-exports.uploadGroupIcon = multer();
-exports.uploadMediaMessage = multer();
+exports.uploadProfilePic = multer({dest:'/'});
+exports.uploadGroupIcon = multer({dest:'/'});
+exports.uploadMediaMessage = multer({dest:'/'});
 
 // exports.uploadProfilePic = multer({dest:`/images/profiles`});
 // exports.uploadGroupIcon = multer({dest:`/images/groupIcons`});
